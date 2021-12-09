8c78f5c03acabf7a1fd4f9d8a0668295693085e4
בר שוורצמן <you@example.com>
2021-12-09 10:54:20 +0200

    10:54 9/12

--- checkForms.js
+++ checkForms.js
@@ -1,5 +1,5 @@
-
-function validateForm(formType) {
+// checking form js
+function validateForm() {
     var x = document.forms["myForm"]["fname"].value;
     var y = document.forms["myForm"]["lname"].value;
     var z = document.forms["myForm"]["phone"].value;
@@ -32,6 +32,49 @@ function validateForm(formType) {
     return true;
 }
 
+
+function validateFormSignUp() {
+    var x = document.forms["myForm"]["Email_Login"].value;
+    var y = document.forms["myForm"]["psw"].value;
+    var z = document.forms["myForm"]["psw-repeat"].value;
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (!x.match(mailformat)){
+        alert("You have entered an invalid email address!");
+        Email_Login.focus();
+        return false;
+    }
+     if (y.length<6) {
+         alert("Password must contain at least 6 chars!");
+         psw.focus();
+         return false;
+     }
+     if (z!=null){
+         if (z != y) {
+          alert("Passwords are not the same");
+          psw-repeat.focus();
+          return false;
+         }
+     }
+    return true;
+}
+
+
+
+// login js
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// Approved form
+function formApproved () {
+    window.location.href = "formApproval.html";
+}
 
 
 
