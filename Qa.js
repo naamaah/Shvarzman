9ef1eb59ca08765be769eafed65fa2b11b04260f
naamaah <naamaah@post.bgu.ac.il>
2021-12-09 12:02:31 +0200

    QA page

---
+++ Qa.js
@@ -0,0 +1,35 @@
+
+
+
+function openQuestion(name) {
+  var x = document.getElementById(name);
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+function openQASub(evt, type) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace("active", "");
+  }
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(type).style.display = "block";
+  evt.currentTarget.className += " active";
+
+
+}
+
