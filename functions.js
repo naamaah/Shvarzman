a313bafa916d6f88659e94a9b2466e3a3895ab14
בר שוורצמן <you@example.com>
2021-12-10 15:54:36 +0200

    /

--- functions.js
+++ functions.js
@@ -1,10 +1,10 @@
 
 // check tour and contact us forms
 function validateForm() {
-    var x = document.forms["myForm"]["fname"].value;
-    var y = document.forms["myForm"]["lname"].value;
-    var z = document.forms["myForm"]["phone"].value;
-    var k = document.forms["myForm"]["email"].value;
+    var x = document.forms["myForm"]["firstname"].value;
+    var y = document.forms["myForm"]["lastname"].value;
+    var z = document.forms["myForm"]["phonenumer"].value;
+    var k = document.forms["myForm"]["emailaddress"].value;
     if (x == "" || y == "") {
         alert("Full name must be filled out");
         fname.focus();
@@ -17,15 +17,15 @@ function validateForm() {
         phone.focus();
         return false;
     }
-    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var mailformat = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;
     if (!k.match(mailformat)){
         alert("You have entered an invalid email address!");
         email.focus();
         return false;
     }
+    alert("The form was successfully received");
     return true;
 }
-
 
 
 // check sign-in form
@@ -73,7 +73,7 @@ function validateFormSignUp() {
           return true;
 }
 
-// log in page:
+// log in page
 // Get the modal
 var modal = document.getElementById('id01');
 
@@ -83,8 +83,6 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
-
-
 
 
 // tour
@@ -112,3 +110,76 @@ if (formType == "tour") {
 
 */
 
+// QA
+function openQuestion(name) {
+  var x = document.getElementById(name);
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function openQASub(evt, type) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace("active", "");
+  }
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(type).style.display = "block";
+  evt.currentTarget.className += " active";
+
+}
+
+//tour
+function openForm() {
+  var x = document.getElementById('gridContainer');
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+// photo gallery
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
