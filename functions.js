aa467afb18c026a5fa8e394355af9f43cc8eb523
naamaah <naamaah@post.bgu.ac.il>
2021-12-11 19:57:43 +0200

    try

--- functions.js
+++ functions.js
@@ -1,3 +1,24 @@
+//main page
+/*function change() {
+var images = [];
+
+images[0] = ['pictures/bazil.jpg'];
+images[1] = ['pictures/green2.jpg'];
+var index = 0;
+
+  document.getElementById("imageMain").src = images[index];
+  if (index == 2) {
+    index = 0;
+  } else {
+    index++;
+  }
+}
+
+window.onload = function () {
+    setInterval(change, 5000);
+};*/
+
+
 
 // check forms inputs
 function checkName(id){
