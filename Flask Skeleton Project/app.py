1376ccd37da105589797ae9060a653c28aabc9bd
shakedhaimi <93468661+shakedhaimi@users.noreply.github.com>
2021-12-27 17:06:17 +0200

    Flask

--- Flask Skeleton Project/app.py
+++ Flask Skeleton Project/app.py
@@ -60,6 +60,8 @@ from pages.page_error_handlers.page_error_handlers import page_error_handlers
 app.register_blueprint(page_error_handlers)
 """
 
+
+
 """
 ###### Components
 ## footer
@@ -70,6 +72,10 @@ app.register_blueprint(footer)
 from components.header.header import header
 app.register_blueprint(header)
 """
+
+
+
+
 
 
 
