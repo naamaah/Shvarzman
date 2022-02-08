95aaa1299919194df017c024da9a68311fde43af
בר שוורצמן <you@example.com>
2022-02-08 21:44:43 +0200

    bar 8/2

--- Flask Skeleton Project/pages/tours/tours.py
+++ Flask Skeleton Project/pages/tours/tours.py
@@ -23,10 +23,9 @@ def not_ordered_dates_list(email):
 
 
 # Routes
-@tours.route('/tours', defaults={'order_check_list': []})
-@tours.route('/tours/<order_check_list>', methods=['GET'])
-def tour_page(order_check_list):
-    print(order_check_list)
+@tours.route('/tours', defaults={'message': ""})
+@tours.route('/tours/<message>', methods=['GET'])
+def tour_page(message):
     tours_list_distinct = Tours.get_tours_dictinct()
     tour_list = Tours.get_all_tours()
     if session.get("email"):
@@ -36,43 +35,46 @@ def tour_page(order_check_list):
         tour_dates= ''
         user_tours= ''
     return render_template('tours.html', tours_list_distinct=tours_list_distinct, tour_list=tour_list,
-                           tour_dates=tour_dates, user_tours=user_tours,order_check_list=order_check_list)
+                           tour_dates=tour_dates, user_tours=user_tours, message=message)
 
 
 
 @tours.route('/tourOrder', methods=['GET'])
 def order_tour():
-    order_check_list = [True, 0, False, False, False]
+    message=''
     if 'tourDate' in request.args:
         email = session["email"]
         tour_dt = request.args["tourDate"]
         num_of_tickets = request.args['numOfTickets']
         current_places_left = Tours.update_places_left(tour_dt, num_of_tickets)
-        check_user_tour = Users_tours.get_user_specific_tour(email, tour_dt)
+        tour_name = Tours.get_tour_name(tour_dt)[0][0]
         if current_places_left < 0:
-            order_check_list[0] = False
-            order_check_list[1] = abs(current_places_left)
+            message = " לצערנו חסרים " + str(abs(current_places_left))+ " מקומות ל" +str(tour_name)+  " בתאריך המבוקש, אנא הירשם מחדש עם כמות כרטיסים תקינה"
         else:
-            order_check_list[2] = True
+            message = "הרשמה ל" +str(tour_name) + " בוצעה בהצלחה, ניפגש בקרוב (:"
             Users_tours.insert_user_tours(email, tour_dt, num_of_tickets)
-    return redirect(url_for('tours.tour_page',order_check_list=order_check_list))
+    return redirect(url_for('tours.tour_page',  message=message))
 
 
 @tours.route('/update_tour_order', methods=['GET'])
 def update_tour():
-    order_check_list = [True, 0, False, False, False]
+    message=''
     if 'tourDateForUpdate' in request.args:
         email = session["email"]
         tour_dt = request.args["tourDateForUpdate"]
         new_num_of_tickets = request.args['updateNumTickets']
         previous_num_of_tickets = int(Users_tours.get_user_specific_tour(email, tour_dt)[0][2])
+        tour_name = Tours.get_tour_name(tour_dt)[0][0]
         if request.args['submit_button'] == 'עדכן כמות כרטיסים':
             difference = int(new_num_of_tickets) - previous_num_of_tickets
+            current_places_left = Tours.update_places_left(tour_dt, difference)
             if Tours.update_places_left(tour_dt, difference) >= 0:
                 Users_tours.update_num_of_tickets(email, tour_dt, new_num_of_tickets)
-                order_check_list[3]=True
+                message = "כמות הכרטיסים ל" + str(tour_name)+ " עודכנה ל-" + str(new_num_of_tickets) + " כרטיסים"
+            else:
+                message = " לצערנו חסרים " + str(abs(current_places_left))+ " מקומות ל" +str(tour_name)+  " בתאריך המבוקש, לכן לא ניתן לבצע עדכון להזמנה"
         if request.args['submit_button'] == 'בטל רישום לסיור':
             Tours.update_places_left(tour_dt, -previous_num_of_tickets)
             Users_tours.delete_user_tour(email, tour_dt)
-            order_check_list[4] = True
-        return redirect(url_for('tours.tour_page',order_check_list=order_check_list))
+            message = "ביטול הזמנה ל" +  str(tour_name)+ " בוצע בהצלחה"
+        return redirect(url_for('tours.tour_page', message=message))
