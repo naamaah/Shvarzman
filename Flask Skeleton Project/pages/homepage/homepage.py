b2406d2f9b09449a73c89f5596332c506b41b4ff
omriGranada <omri.granada@gmail.com>
2022-02-09 20:57:07 +0200

    omri

--- Flask Skeleton Project/pages/homepage/homepage.py
+++ Flask Skeleton Project/pages/homepage/homepage.py
@@ -1,4 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for
+from utilities.db.recipes import Recipes
+import random
+
 
 # homepage blueprint definition
 homepage = Blueprint('homepage', __name__,
@@ -10,7 +13,20 @@ homepage = Blueprint('homepage', __name__,
 # Routes
 @homepage.route('/')
 def index():
-    return render_template('homepage.html')
+    recipes = Recipes.getAllRecipes()
+    size = len(recipes)
+    print(size)
+    recipe_id = random.randint(0,size-1)
+    print(recipe_id)
+    recipe_name = recipes[recipe_id].recipe_name
+    recipe_description = recipes[recipe_id].description
+    recipe_pic = recipes[recipe_id].picture
+    print(recipe_name)
+    print(recipe_description)
+    print(recipe_pic)
+    return render_template('homepage.html',recipe_name = recipe_name,
+                           recipe_description = recipe_description,
+                           recipe_pic = recipe_pic)
 
 @homepage.route('/homepage', defaults={'firstLog': False})
 @homepage.route('/homepage')
